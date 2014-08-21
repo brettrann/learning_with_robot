54a8ec5a0af1e7ed0fa2e9f8b4ec0135f55b2138
Brett Rann <brettrann@gmail.com>
2014-08-21 15:16:40 +1000

    add robot object with heading and rotate support, and tests

---
+++ spec/robot_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+# XXX this below require doesn't seem necessary in online examples. config missing?
+require './lib/robot.rb'
+
+describe "Robot object" do
+
+  @robot
+  before :each do
+    @robot = Robot.new()
+  end
+
+  it 'when setting the heading' do
+    expect(@robot.heading(:north)).to equal :north
+    expect(@robot.heading(:east )).to equal :east
+    expect(@robot.heading(:south)).to equal :south
+    expect(@robot.heading(:west )).to equal :west
+    expect(@robot.heading(:oops )).to equal nil
+  end
+
+  it 'when rotating right' do
+    expect(@robot.rotate_right()).to equal nil
+    expect(@robot.heading(:north)).to equal :north
+    expect(@robot.rotate_right()).to equal :east
+    expect(@robot.rotate_right()).to equal :south
+    expect(@robot.rotate_right()).to equal :west
+    expect(@robot.rotate_right()).to equal :north
+  end
+
+  it 'when rotating left' do
+    expect(@robot.rotate_left()).to equal nil
+    expect(@robot.heading(:north)).to equal :north
+    expect(@robot.rotate_left()).to equal :west
+    expect(@robot.rotate_left()).to equal :south
+    expect(@robot.rotate_left()).to equal :east
+    expect(@robot.rotate_left()).to equal :north
+  end
+end
