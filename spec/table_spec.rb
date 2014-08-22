948837cefc63f5a8389f51ec1dc44bca539fb3ed
Brett Rann <brann@gmail.com>
2014-08-22 12:16:38 +1000

    add attr reader for x,y on table

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -13,13 +13,19 @@ describe "Table object" do
 
   it 'when placing the robot' do
     expect(@table.place(@robot, 0, 0)).to equal @robot
+    expect(@table.x                  ).to equal 0
+    expect(@table.y                  ).to equal 0
     expect(@table.place(@robot, 4, 4)).to equal @robot
+    expect(@table.x                  ).to equal 4
+    expect(@table.y                  ).to equal 4
     expect(@table.place(@robot, 9, 9)).to equal nil
     expect(@table.place(@robot, 5, 0)).to equal nil
     expect(@table.place(@robot, 0, 5)).to equal nil
 
     @table = Table.new(100,100)
     expect(@table.place(@robot,  99,  99)).to equal @robot
+    expect(@table.x                      ).to equal 99
+    expect(@table.y                      ).to equal 99
     expect(@table.place(@robot, 100,  99)).to equal nil
     expect(@table.place(@robot, 100, 100)).to equal nil
   end
