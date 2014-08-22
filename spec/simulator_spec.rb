5df2eedbe758a5148ba713444b216b6541af3aa0
Brett Rann <brann@gmail.com>
2014-08-22 12:31:38 +1000

    test invalid place on table

--- spec/simulator_spec.rb
+++ spec/simulator_spec.rb
@@ -20,11 +20,16 @@ describe 'Simulator object' do
     expect(@simulator.execute('REPORT'         )).to eq('2,2,NORTH')
   end
 
+  it 'place robot on table with invalid coordinates' do
+    expect(@simulator.execute('PLACE 5,5,NORTH')).to equal(nil)
+    expect(@simulator.execute('REPORT'         )).to equal(nil)
+  end
+
   it 'rotate robot' do
     expect(@simulator.execute('PLACE 2,2,NORTH')).to be_a(Robot)
     expect(@simulator.execute('REPORT'         )).to eq('2,2,NORTH')
-    expect(@simulator.execute('LEFT')).to equal(:west)
-    expect(@simulator.execute('RIGHT')).to equal(:north)
+    expect(@simulator.execute('LEFT')           ).to equal(:west)
+    expect(@simulator.execute('RIGHT')          ).to equal(:north)
   end
 
 end
