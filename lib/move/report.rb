0b6bd23e7ce89990c9df65deea508aae6e16078d
Brett Rann <brettrann@gmail.com>
2014-08-23 16:22:36 +1000

    resolve merge from foolhardy use of reset

--- lib/move/report.rb
+++ lib/move/report.rb
@@ -2,11 +2,7 @@ require 'Robot'
 
 class Report < Move
 
-<<<<<<< HEAD
   Regexp = /^REPORT$/i
-=======
-  Regexp = /^REPORT$/
->>>>>>> e9495566b3819b30d8f736e3825cdb66a5fc7fe1
 
   def execute(command)
     return "#{@table.x},#{@table.y},#{@robot.heading}".upcase if self.is_active
