f461035b5ce80dd256fe92d8fa8fac9154381bb6
Brett Rann <brann@gmail.com>
2014-08-27 09:52:30 +1000

    point class implemented

--- lib/point.rb
+++ lib/point.rb
@@ -1,3 +1,18 @@
 class Point
 
+  attr_accessor :x, :y
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def inside(bound_a, bound_b)
+    if @x >= bound_a.x && @x <= bound_b.x
+      @y >= bound_a.y && @y <= bound_b.y
+    elsif @x >= bound_b.x && @x <= bound_a.x
+      @y >= bound_b.y && @y <= bound_a.y
+    end
+  end
+
 end
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -3,17 +3,19 @@ require 'Point'
 describe 'Point object' do
 
     it 'tests instantiate coordiantes' do
-      coordintes = Point.new 0,0
-      expects([coordinates.x, coordinates.y]).to eq([0,0])
+      point = Point.new 0,0
+      expect([point.x, point.y]).to eq([0,0])
+      point = Point.new 8,-5
+      expect([point.x, point.y]).to eq([8,-5])
     end
     it 'tests in_bounds' do
       bound_low = Point.new 0,0
       bound_low_negative = Point.new -10,-10
       bound_high = Point.new 10,10
-      coordinates = Point.new 5,5
-      expect(coordinates.in_bounds(bound_low, bound_high)).to equal true
-      expect(coordinates.in_bounds(bound_high, bound_low)).to equal true
-      expect(coordinates.in_bounds(bound_low_negative, bound_high)).to equal true
-      expect(coordinates.in_bounds(bound_high, bound_low_negative)).to equal true
+      point = Point.new 5,5
+      expect(point.inside(bound_low, bound_high)).to equal true
+      expect(point.inside(bound_high, bound_low)).to equal true
+      expect(point.inside(bound_low_negative, bound_high)).to equal true
+      expect(point.inside(bound_high, bound_low_negative)).to equal true
     end
 end
