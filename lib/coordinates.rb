fcad726305f80b83641d1166afc5a7d102ced374
Brett Rann <brann@gmail.com>
2014-08-27 09:35:08 +1000

    rename coordintes to point cos lazy

--- lib/coordinates.rb
+++
@@ -1,3 +0,0 @@
-class Coordinates
-
-end
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,16 +1,16 @@
-require 'Coordinates'
+require 'Point'
 
-describe 'Coordinates object' do
+describe 'Point object' do
 
     it 'tests instantiate coordiantes' do
-      coordintes = Coordinates.new 0,0
+      coordintes = Point.new 0,0
       expects([coordinates.x, coordinates.y]).to eq([0,0])
     end
     it 'tests in_bounds' do
-      bound_low = Coordinates.new 0,0
-      bound_low_negative = Coordinates.new -10,-10
-      bound_high = Coordinates.new 10,10
-      coordinates = Coordinates.new 5,5
+      bound_low = Point.new 0,0
+      bound_low_negative = Point.new -10,-10
+      bound_high = Point.new 10,10
+      coordinates = Point.new 5,5
       expect(coordinates.in_bounds(bound_low, bound_high)).to equal true
       expect(coordinates.in_bounds(bound_high, bound_low)).to equal true
       expect(coordinates.in_bounds(bound_low_negative, bound_high)).to equal true
